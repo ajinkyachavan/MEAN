b81eed6919c2f7ea805941cca620561a24192b7a
ajinkyachavan <a.ajinkya9030@gmail.com>
2017-09-02 03:59:48 -0400

    latest

--- src/app/map-india/map-india.component.ts
+++ src/app/map-india/map-india.component.ts
@@ -3,7 +3,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
-import {Observable} from 'rxjs/Rx';
+import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import { DataService } from '../services/data.service';
@@ -20,156 +20,166 @@ import { MapsAPILoader } from '@agm/core';
   styleUrls: ['./map-india.component.css']
 })
 
-export class MapIndiaComponent  {
+export class MapIndiaComponent {
 
 
-  
+
   title = 'Test';
   DataArray: any = [];
-
+  result: any = [];
   public latitude: number;
   public longitude: number;
   public searchControl: FormControl;
   public zoom: number;
+  isClicked: boolean = false;
 
-  @ViewChild("search")
+  @ViewChild('search')
   public searchElementRef: ElementRef;
 
 
   id = 'qDuKsiwS5xw';
-  width = window.innerWidth*0.6;
-  height = window.innerHeight*0.5;
+  width = window.innerWidth * 0.4;
+  height = window.innerHeight * 0.5;
   private player;
   private ytEvent;
-  
-  constructor(public data: DataService,private mapsAPILoader: MapsAPILoader, private ngZone: NgZone){
+
+  constructor(public data: DataService, private mapsAPILoader: MapsAPILoader, private ngZone: NgZone) {
 
 
-  this.loadTableData();
+    this.loadTableData();
 
-  
-  // $(function(){
-  //   $('#example').DataTable();
-  //   });
+    console.log('myArr',this.DataArray);
+
+
+
+    // $(function(){
+    //   $('#example').DataTable();
+    //   });
 
   }
 
   ngOnInit(): void {
-     //set google maps defaults
-     this.zoom = 4;
-     this.latitude = 39.8282;
-     this.longitude = -98.5795;
-  
+    //set google maps defaults
+    this.zoom = 4;
+    this.latitude = 39.8282;
+    this.longitude = -98.5795;
 
-     //create search FormControl
-     this.searchControl = new FormControl();
- 
-     //set current position
-     this.setCurrentPosition();
 
-     
- 
-     //load Places Autocomplete
-     this.mapsAPILoader.load().then(() => {
+    //create search FormControl
+    this.searchControl = new FormControl();
+
+    //set current position
+    this.setCurrentPosition();
+
+
+
+    //load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
       let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
-         types: ['address'] //address  [ '(cities)', '(regions)', 'country', 'postal_code', 'sublocality', 'establishment', 'address', 'geocode'] 
+        types: ['address'] //address  [ '(cities)', '(regions)', 'country', 'postal_code', 'sublocality', 'establishment', 'address', 'geocode']
 
-       });
-       autocomplete.addListener("place_changed", () => {
-         this.ngZone.run(() => {
-           //get the place result
-           let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+      });
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
           //  service = new google.maps.places.PlacesService(map);
           //  service.nearbySearch(request, callback);
           //  //verify result
-           if (place.geometry === undefined || place.geometry === null) {
-             return;
-           }
- 
-           //set latitude, longitude and zoom
-           this.latitude = place.geometry.location.lat();
-           this.longitude = place.geometry.location.lng();
-           this.zoom = 15;
-           
-          });
-       }); 
-     });   
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          //set latitude, longitude and zoom
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.zoom = 15;
+
+        });
+      });
+    });
   }
 
+  changeId(newId){
+    this.id = newId;
+    console.log(this.id);
+    this.player.loadVideoById(this.id);  }
 
   private setCurrentPosition(newPosition: any = null) {
-    console.log(newPosition+" newPosition")
+    console.log(newPosition + " newPosition")
     if ("geolocation" in navigator && newPosition == null) {
       navigator.geolocation.getCurrentPosition((position) => {
         this.latitude = position.coords.latitude;
         this.longitude = position.coords.longitude;
         this.zoom = 12;
-        
+
       });
-    }else{
+    } else {
       var myPosition = new Position();
       myPosition = newPosition;
 
-      console.log(newPosition.latitude,newPosition.longitude," newPosition andar")
-      
+      console.log(newPosition.latitude, newPosition.longitude, " newPosition andar")
+
       this.latitude = myPosition.coords.latitude;
       this.longitude = myPosition.coords.longitude;
       this.zoom = 12;
 
-      
+
     }
   }
-  
-  loadTableData(){
+
+  loadTableData() {
     this.data.loadData().subscribe(
       data => {
         this.DataArray = data;
-        //console.log(data);
+        console.log(data);
       }
     );
   }
 
-  placeMarker($event){
+  placeMarker($event) {
     console.log($event.coords.lat);
     console.log($event.coords.lng);
     this.latitude = $event.coords.lat;
     this.longitude = $event.coords.lng;
   }
 
-  cityData(data){
-    //console.log('printing data - '+data.target.innerText);
-    var result = this.DataArray[0];
+  cityData(data) {
 
-    for (var i = 0; i < this.DataArray.length; i++) { 
+  this.isClicked = true;
+    //console.log('printing data - '+data.target.innerText);
+    this.result = this.DataArray[0];
+
+    for (var i = 0; i < this.DataArray.length; i++) {
       //console.log(this.DataArray[i].title,data.target.innerText, this.DataArray[i].title === data.target.innerText, typeof(this.DataArray[i].title), typeof(data.target.innerText));
-      if(this.DataArray[i].title === data.target.innerText){
-        result = this.DataArray[i];
+      if (this.DataArray[i].title === data.target.innerText) {
+        this.result = this.DataArray[i];
       }
     }
     //console.log(result)
     document.getElementById('placeDiv').style.display = 'block';
-    
-    document.getElementById("frameImg").setAttribute('src', result.imageUrl);
-    document.getElementById("frameTitle").innerText = result.bio;
-    document.getElementById("framePara").innerText = result.description;
-    
-    this.latitude = parseFloat(result.latitude);
-    this.longitude = parseFloat(result.longitude);
-   // this.setCurrentPosition(result);
+
+    document.getElementById("frameImg").setAttribute('src', this.result.imageUrl);
+    document.getElementById("frameTitle").innerText = this.result.bio;
+    document.getElementById("framePara").innerText = this.result.description;
+
+    this.latitude = parseFloat(this.result.latitude);
+    this.longitude = parseFloat(this.result.longitude);
+    // this.setCurrentPosition(result);
     // this.latitude = result.latitude;
     // this.longitude = result.longitude;
     // this.zoom = 12;
     // //console.log(document.getElementById("frameImg").getAttribute("src"))
-     /*
-    document.getElementById("placeDiv").style.border = "1px solid #808080";
-    document.getElementById("placeDiv").style.margin = "30px";
-    document.getElementById("placeDiv").style.padding = "15px";
-    if(result != null)    
-      document.getElementById("placeDiv").appendChild(p).innerText = result.title;
-    else
-      document.getElementById("placeDiv").appendChild(p).innerText = data.target.innerText;
-    */
-  
+    /*
+   document.getElementById("placeDiv").style.border = "1px solid #808080";
+   document.getElementById("placeDiv").style.margin = "30px";
+   document.getElementById("placeDiv").style.padding = "15px";
+   if(result != null)
+     document.getElementById("placeDiv").appendChild(p).innerText = result.title;
+   else
+     document.getElementById("placeDiv").appendChild(p).innerText = data.target.innerText;
+   */
+
   }
 
 
@@ -179,11 +189,11 @@ export class MapIndiaComponent  {
   savePlayer(player) {
     this.player = player;
   }
-  
+
   playVideo() {
     this.player.playVideo();
   }
-  
+
   pauseVideo() {
     this.player.pauseVideo();
   }
